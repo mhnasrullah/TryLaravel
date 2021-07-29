fbb109d780e87c93b5fbbd7797cb60c9f8ae5023
Hasan Nasrullah <hnasrul22@gmail.com>
2021-07-29 18:47:07 +0700

    init

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            
+        </div>
+    </div>
+    <div class="row">
+        <div class="col"></div>
+    </div>
+</div>
+
+</x-app-layout>
